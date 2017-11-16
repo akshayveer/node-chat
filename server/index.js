bfa19ca3cd747716d49b380052fdd0914eaafe6b
akshayveer <akshayveer096@gmail.com>
2017-11-16 20:02:06 +0530

    wire up socket.io library

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', socket => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('client was disconnected');
+  });
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
